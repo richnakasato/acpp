23c29d4bd4df2f736b516e47a45669e8c04c92fe
Rich Nakasato <richnakasato@hotmail.com>
2019-01-09 09:52:05 -0500

    updated with comments

--- ch1/scope_test.cpp
+++ ch1/scope_test.cpp
@@ -3,9 +3,15 @@
 
 int main()
 {
-    { const std::string s = "a string";
+    { const std::string s = "a string";         // separate scopes, okay
       std::cout << s << std::endl; }
     { const std::string s = "another string";
       std::cout << s << std::endl; }
+
+    { const std::string s = "a string";         // this works because of hidden
+      std::cout << s << std::endl;              // scope, think of variable
+    { const std::string s = "another string";   // inside function with same
+      std::cout << s << std::endl; };}          // name as calling function var
+
     return 0;
 }
