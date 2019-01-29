89e24e7bc799536ee38b93fd2100974d286d051f
Rich Nakasato <richnakasato@hotmail.com>
2019-01-29 17:04:47 -0500

    working through ch6

--- ch6/finding_urls.cpp
+++ ch6/finding_urls.cpp
@@ -1,6 +1,16 @@
 #include <string>
 #include <vector>
 
+std::string::const_iterator url_beg(std::string::const_iterator b,
+                                    std::string::const_iterator e)
+{
+}
+
+std::string::const_iterator url_end(std::string::const_iterator b,
+                                    std::string::const_iterator e)
+{
+}
+
 std::vector<std::string> find_urls(const std::string& str)
 {
     std::vector<std::string> result;
